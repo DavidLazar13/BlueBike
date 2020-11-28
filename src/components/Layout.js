7fa3516b42681abb652fb1bc6860317edea38678
David <david@remote-tech.co.uk>
2020-11-28 16:59:37 +0200

    pushpula

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
-    color: 'black',
-    pula: 'este'
+  color: 'black',
+  pula: 'este',
 };
 
 function Layout(props) {
-    const { children } = props;
-    return (
-        <ThemeProvider theme={theme}>
-            {children}
-        </ThemeProvider>
-    )
+  const { children } = props;
+  return (
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  );
 }
-
-
 
 export default Layout;
