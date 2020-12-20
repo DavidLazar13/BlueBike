b853edbb2e301dc8da914a050f586231214ffc18
raul vladau <alurduvaal@rauls-iMac.local>
2020-12-20 22:47:37 +0200

    initial commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,16 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { graphql } from 'gatsby';
 
-export const CONTACT_QUERY = graphql`
-query CONTACT_QUERY {
-  contentfulContact {
-    adress
-    phone
-    eMail
-  }
-}
-`
-
 const Adress = styled.p`
 color: ${({ theme }) => theme.colors.primary};
 font-weight: 400;
@@ -27,6 +17,16 @@ font-size: 12px;
 line-height: 13px;
 letter-spacing: 6px;
 `;
+
+export const CONTACT_QUERY = graphql`
+query CONTACT_QUERY {
+  contentfulContact {
+    adress
+    phone
+    eMail
+  }
+}
+`
 
 function Contact({data} ) {
     const {contentfulContact} = data;
