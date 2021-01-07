590081fe9bcb7023cd17d752eb89e292007c5dac
David <david@remote-tech.co.uk>
2021-01-07 22:58:39 +0200

    google maps implementation

---
+++ src/components/MapPin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const MapPin = () => (
+  <div>
+    <img src="/map-pin.svg" width="30px" />
+  </div>
+);
+
+export default MapPin;
